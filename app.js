7d1cb6afeaebeb4e0e5afda3e14e0606ebf74557
Auh3b <ngaiyayec@yahoo.com>
2020-12-31 20:29:19 +0200

    fix image

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ function update(e) {
   xhr.onload = function () {
     if (this.status === 200) {
       const data = JSON.parse(this.responseText);
-      console.log(data);
+      console.log(data.countryInfo.flag);
 
       const output = `
-                <img src:${data.countryInfo.flag}>
-                <li>Country: ${data.country}</li>
+                
+                <li>Country: ${data.country} <img src="${data.countryInfo.flag}"/> </li>
                 <li>Cases: ${data.cases}</li>
                 <li>Recovery: ${data.recovered}</li>
                 <li>Deaths: ${data.deaths}</li>
@@ -39,12 +39,12 @@ function update(e) {
 function getMore(e) {
   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "covid_data.json", true);
+  xhr.open("GET", "covid_data.geojson", true);
 
   xhr.onload = function () {
     if (this.status === 200) {
       const cases = JSON.parse(this.responseText);
-      console.log(cases);
+      console.log(cases.features);
       const output2 = `<table class = "u-full-width">
                   <thead>
                       <th>District</th>
@@ -58,12 +58,12 @@ function getMore(e) {
 
       let dataset = "";
 
-      cases.forEach(function (number) {
+      cases.features.forEach(function (number) {
         dataset += `<tr>
-                    <td>${number.districts}</td>
-                    <td>${number.cases}</td>
-                    <td>${number.deaths}</td>
-                    <td>${number.recoveries}</td>
+                    <td>${number.properties.district}</td>
+                    <td>${number.properties.cases}</td>
+                    <td>${number.properties.deaths}</td>
+                    <td>${number.properties.recoveries}</td>
                     </tr>
                     `;
       });
