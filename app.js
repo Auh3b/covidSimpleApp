c8f94f4b1ff08d6b6eab7d420d93b46c096e8538
Auh3b <ngaiyayec@yahoo.com>
2021-04-23 19:58:48 +0200

    updated data with MOH api

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.getElementById("get-more").addEventListener("click", getMore);
+// document.getElementById("get-more").addEventListener("click", getMore);
 
 function update() {
   const dailyUpdate = new Update();
@@ -6,117 +6,108 @@ function update() {
   dailyUpdate
     .get("https://disease.sh/v3/covid-19/countries/malawi?strict=true")
     .then((data) => {
-      const daily = ` 
-                        <div class="card"  >
-                        <h5  class="negative">
-                            Total Cases
-                        </h5>
-                        <p>
-                            ${data.cases}
-                            <span class="clr-neg">(
-                            <i class="fas fa-caret-up"></i>
-                            ${data.todayCases}
-                            )</span>
-                        </p>
-                        </div>           
-                        <div class="card" >
-                        <h5 class="negative">
-                            Active Cases
-                        </h5>
-                        <p>
+      const totalCases = document.getElementById("totalCases");
+      const active = document.getElementById("active");
+      const recovery = document.getElementById("recovery");
+      const deaths = document.getElementById("deaths");
+
+      totalCases.innerHTML = `<p>
+                                ${data.cases}
+                                <span class="clr-neg">(
+                                <i class="fa fa-caret-up"></i>
+                                ${data.todayCases}
+                                )</span>
+                            </p>
+                            `;
+
+      active.innerHTML = `
+                            <p>
                             ${data.active} 
                             <span class="clr-neg">(
-                            <i class="fas fa-caret-up"></i>
+                            <i class="fa fa-caret-up"></i>
                             ${data.todayCases}
                             )</span>
                         </p>
-                        </div> 
-
-                        <div class="card" >
-                        <h5 class="positive">
-                            Total Recovered
-                        </h5>
-                        <p>
+                          `;
+      recovery.innerHTML = `
+                            <p>
                             ${data.recovered}
                             <span class="clr-pos">(
-                            <i class="fas fa-caret-up"></i>
+                            <i class="fa fa-caret-up"></i>
                             ${data.todayRecovered}
                             )</span>
                         </p>
-                        </div>
-                        
-                        <div class="card" >
-                        <h5 class="negative">
-                            Total Deaths
-                        </h5>
-                        <p>
+                            `;
+      deaths.innerHTML = `
+                          <p>
                             ${data.deaths}
                             <span class="clr-neg">(
-                            <i class="fas fa-caret-up"></i>
+                            <i class="fa fa-caret-up"></i>
                             ${data.todayDeaths}
                             )</span>
                         </p>
-                        </div> 
-        `;
-      document.getElementById("content").innerHTML = daily;
+                          `;
     })
     .catch((err) => console.log(err));
 }
 
 update();
 
-function getMore() {
-  const table = new Update();
+const table = new Update();
 
-  table
-    .get("covid_data.geojson")
-    .then((data) => {
-      const tableStr = `<table class = "u-full-width">
-                                <thead>
-                                    <th>District</th>
-                                    <th>Cases</th>
-                                    <th>Deaths</th>
-                                    <th>Recovery</th>
-                                </thead>
-                                <tbody id= "cases">
-                                </tbody>
-                            </table>`;
-      let tableData = "";
-      data.features.forEach(function (feature) {
-        tableData += `<tr>
-              <td>${feature.properties.district}</td>
-              <td>${feature.properties.cases}</td>
-              <td>${feature.properties.deaths}</td>
-              <td>${feature.properties.recoveries}</td>
-              </tr>
-              `;
-      });
-      document.getElementById("table").innerHTML = tableStr;
-      document.getElementById("cases").innerHTML = tableData;
+const tablelize = (data) => {
+  const tableStr = `<table class = "table table-striped table-responsive-sm mx-auto">
+      <thead class="thead-dark">
+          <th>District</th>
+          <th>Active Cases</th>
+          <th>Total Cases</th>
+          <th>Total Deaths</th>
+          <th>Total Recovery</th>
+      </thead>
+      <tbody id= "cases">
+      </tbody>
+    </table>`;
+  let tableData = "";
 
-      const updateDate = "Last Update:<em> 19/01/2021</em>";
-      document.getElementById("update-date").innerHTML = updateDate;
-    })
-    .catch((err) => console.log(err));
-}
+  data.districts.sort((a, b) => {
+    return b.numberOfConfirmedCases - a.numberOfConfirmedCases;
+  });
+  data.districts.forEach(function (feature) {
+    tableData += `<tr>
+    <td>${feature.districtName}</td>
+    <td>${feature.numberOfActiveCases}</td>
+    <td>${feature.numberOfConfirmedCases}</td>
+    <td>${feature.numberOfConfirmedDeaths}</td>
+    <td>${feature.numberOfRecoveredPatients}</td>
+    </tr>
+    `;
+  });
+  document.getElementById("table").innerHTML = tableStr;
+  document.getElementById("cases").innerHTML = tableData;
+};
 
-// add graph
-// function getChart(e) {
-//   let chart =
-//     '<iframe width="950" height="600" frameborder="0" scrolling="no" src="//plotly.com/~robChiko/28.embed"></iframe>';
+const lastUpdated = (data) => {
+  let date = new Date(data.lastUpdate);
 
-//   document.getElementById("chart").innerHTML = chart;
-// }
-// getChart();
+  document.querySelector(
+    ".updated"
+  ).textContent = `Last Update: ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} `;
+};
 
-// get table
+table
+  .get("https://covid19.health.gov.mw:3000/api/v0/districts/aggregates")
+  .then((data) => {
+    tablelize(data);
+    lastUpdated(data);
+  })
+  .catch((err) => console.log(err));
 
 // scrolling
 $(document).scroll(function () {
   var y = $(this).scrollTop();
-  if (y > 800) {
-    $("#backer a").fadeIn();
+  if (y > 500) {
+    $("#backer").fadeIn();
   } else {
-    $("#backer a").fadeOut();
+    $("#backer").fadeOut();
   }
 });
--- map.js
+++ map.js
@@ -1,147 +1,128 @@
-document.getElementById("get-map").addEventListener("click", loadMap);
+const renderMap = (data) => {
+  let mapOptions = {
+    center: [-13.2512161, 34.3015278],
+    zoom: 6,
+  };
 
-function loadMap() {
-  const mapUpdate = new Update();
+  let mapDiv = `
+                <div id="map"  style=" margin: auto;">
+                </div>
+                `;
 
-  mapUpdate
-    .get("covid_data.geojson")
-    .then((data) => {
-      let mapOptions = {
-        center: [-13.2512161, 34.3015278],
-        zoom: 6,
-      };
+  document.getElementById("mapid").innerHTML = mapDiv;
 
-      let mapDiv = `
-                    <div id="map" class="card" style=" margin: auto;">
-                    </div>
-                    `;
+  let map = new L.map("map", mapOptions);
 
-      document.getElementById("mapid").innerHTML = mapDiv;
+  let layer = new L.TileLayer(
+    "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.wmflabs.org">WikiMediax</a>',
+      maxZoom: 8,
+      minZoom: 6,
+    }
+  );
 
-      let map = new L.map("map", mapOptions);
+  map.addLayer(layer);
 
-      let layer = new L.TileLayer(
-        "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",
-        {
-          attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-            'Imagery © <a href="https://www.wmflabs.org">WikiMediax</a>',
-          maxZoom: 8,
-          minZoom: 6,
-        }
-      );
+  function getColor(d) {
+    return d > 4000
+      ? "#0A4154"
+      : d > 2500
+      ? "#295969"
+      : d > 1000
+      ? "#47717F"
+      : d > 500
+      ? "#668894"
+      : d > 150
+      ? "#85A0AA"
+      : "#A3B8BF";
+  }
 
-      map.addLayer(layer);
+  function casesOptions(feature) {
+    return {
+      color: "#225B0B",
+      fillColor: getColor(feature.properties.cases),
+      weight: 0.5,
+      fillOpacity: 0.8,
+      dashArray: "1",
+    };
+  }
 
-      function getColor(d) {
-        return d > 4000
-          ? "#0A4154"
-          : d > 2500
-          ? "#295969"
-          : d > 1000
-          ? "#47717F"
-          : d > 500
-          ? "#668894"
-          : d > 150
-          ? "#85A0AA"
-          : "#A3B8BF";
-      }
+  function highlightFeature(e) {
+    let layer = e.target;
 
-      function casesOptions(feature) {
-        return {
-          color: "#225B0B",
-          fillColor: getColor(feature.properties.cases),
-          weight: 0.5,
-          fillOpacity: 0.8,
-          dashArray: "1",
-        };
-      }
+    layer.setStyle({
+      weight: 2,
+      color: "#316D2E",
+    });
 
-      function highlightFeature(e) {
-        let layer = e.target;
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
+    }
+  }
 
-        layer.setStyle({
-          weight: 2,
-          color: "#316D2E",
-        });
+  let geojson;
 
-        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-          layer.bringToFront();
-        }
-        info.update(layer.feature.properties);
-      }
+  function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+  }
+  let layerPopupInfo = (feature) => {
+    return `
+                          <div class="layerPopupInfo">
+                            <p class="lead">${feature.properties.district}</p>
+                            <p>Cases: ${feature.properties.cases}</p>
+                            <p>Deaths:${feature.properties.deaths}</p>
+                            <p>Recoveries: ${feature.properties.recoveries}</p>
+                          </div>
+                          `;
+  };
 
-      let geojson;
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(layerPopupInfo(feature));
+    layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+    });
+  }
 
-      function zoomToFeature(e) {
-        map.fitBounds(e.target.getBounds());
-      }
+  geojson = L.geoJSON(data, {
+    style: casesOptions,
+    onEachFeature: onEachFeature,
+  }).addTo(map);
 
-      function resetHighlight(e) {
-        geojson.resetStyle(e.target);
-        info.update();
-      }
+  let legend = L.control({ position: "bottomleft" });
 
-      function onEachFeature(feature, layer) {
-        layer.on({
-          mouseover: highlightFeature,
-          mouseout: resetHighlight,
-          click: zoomToFeature,
-        });
-      }
+  legend.onAdd = function (map) {
+    let div = L.DomUtil.create("div", "info legend d-flex"),
+      grades = [0, 150, 500, 1000, 2500, 4000],
+      labels = [];
 
-      geojson = L.geoJSON(data, {
-        style: casesOptions,
-        onEachFeature: onEachFeature,
-      }).addTo(map);
-      // console.log(cases);
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML += `<span class="legendItem">
+                        <i class="m-0 w-100" style="background: ${getColor(
+                          grades[i] + 1
+                        )}"></i>  
+                        <span class="py-1">${
+                          grades[i] +
+                          (grades[i + 1] ? "&ndash;" + grades[i + 1] : "+")
+                        }</span>         
+                      </span>`;
+    }
 
-      let info = L.control();
+    return div;
+  };
 
-      info.onAdd = function (map) {
-        this._div = L.DomUtil.create("div", "info");
-        this.update();
-        return this._div;
-      };
+  legend.addTo(map);
+  console.log(data);
+};
 
-      info.update = function (props) {
-        this._div.innerHTML =
-          "<h4>Malawi Covid-19 <br> Statistics</h4>" +
-          (props
-            ? "<h5>" +
-              "<b>" +
-              props.district +
-              "</b></h5>" +
-              `<p>Case: ${props.cases}` +
-              "</p>" +
-              `<p>Deaths: ${props.deaths}` +
-              "</p>" +
-              `<p>Recovery: ${props.recoveries}</p>`
-            : "Hover over district");
-      };
+const mapUpdate = new Update();
 
-      info.addTo(map);
-
-      let legend = L.control({ position: "bottomleft" });
-
-      legend.onAdd = function (map) {
-        let div = L.DomUtil.create("div", "info legend"),
-          grades = [0, 150, 500, 1000, 2500, 4000],
-          labels = [];
-
-        for (var i = 0; i < grades.length; i++) {
-          div.innerHTML +=
-            '<i style="background:' +
-            getColor(grades[i] + 1) +
-            '"></i> ' +
-            grades[i] +
-            (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-        }
-
-        return div;
-      };
-
-      legend.addTo(map);
-    })
-    .catch((err) => console.log(err));
-}
+mapUpdate
+  .get("covid_data.geojson")
+  .then((data) => {
+    renderMap(data);
+  })
+  .catch((err) => console.log(err));
